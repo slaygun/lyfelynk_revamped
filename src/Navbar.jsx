c1e1c484f98992eff09f0557d4a2b5d2a73bd29a
slaygun <nabhraghav@gmail.com>
2024-06-19 20:30:55 +0530

    navbar always on top

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -13,19 +13,24 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="p-4 w-full z-10 top-0 shadow-md space-y-2">
-      <div className="px-6 py-4 container mx-auto flex justify-between items-center rounded-2xl border border-gray-700 shadow-md">
+    <nav className="fixed p-4 w-full z-20 top-0 space-y-2">
+        
+      <div class="circlePosition w-11/12 h-11/12 bg-[#367ed142] rounded-[100%] absolute -z-10 blur-[100px] flex justify-center items-center">
+        <div class="circle w-[17rem] h-[17rem] bg-[#5743ee42] rounded-[100%]" />
+      </div>
+
+      <div className="px-6 py-4 container mx-auto flex justify-between items-center rounded-2xl border border-gray-700 shadow-lg backdrop-blur-lg">
         {/* <img 
             src='/assets/lyfelynk.png'
             alt="LyfeLynk"
             className='w-36 md:w-44'
         /> */}
-        <div className='flex items-center space-x-2'>
+        <button className='flex items-center space-x-2 click-pointer' onClick={() => scroll.scrollToTop()}>
             <Heart className='text-blue-600 w-8 h-8'/>
             <span className='font-bold text-3xl font-prompt'>LyfeLynk</span>
-        </div>
+        </button>
         <div className="hidden md:flex space-x-6 items-center">
-          {Object.keys(links).map((key) => (
+            {Object.keys(links).map((key) => (
             <Link
               key={key}
               activeClass="active"
@@ -59,7 +64,7 @@ const Navbar = () => {
           )}
         </div>
       </div>
-      <div className={`md:hidden px-6 py-4 border border-gray-700 rounded-2xl transition-all duration-500 ease-in-out ${isOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0 overflow-hidden'}`}>
+      <div className={`md:hidden px-6 py-4 border border-gray-700 backdrop-blur-xl rounded-2xl transition-all duration-500 ease-in-out ${isOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0 overflow-hidden'}`}>
         {Object.keys(links).map((key) => (
           <Link
             key={key}
@@ -69,7 +74,7 @@ const Navbar = () => {
             smooth={true}
             offset={-70}
             duration={500}
-            className="block text-center text-gray-400 hover:text-blue-600 font-black text-sm hover:underline cursor-pointer py-2"
+            className="block text-center text-white hover:text-blue-600 font-black text-sm hover:underline cursor-pointer py-2"
             onClick={() => setIsOpen(false)}
           >
             {links[key]}
@@ -87,6 +92,10 @@ const Navbar = () => {
           </span>
         </a>
       </div>
+      
+      {/* Blob */}
+      {/* <div className="absolute top-80 right-80 w-64 h-64 bg-gradient-to-bl from-sky-500 via-grey-800 to-black-700 rounded-full blur-3xl -z-10"></div> */}
+
     </nav>
   );
 };
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,15 @@
-
 const Hero = () => {
-  return (
-    <div>
-      huh
-    </div>
-  );
-};
-
-export default Hero;
+    return (
+      <section className="mt-32 md:mt-36">
+        <div className="container mx-auto flex justify-center">
+          <img 
+            src="/assets/heroblock_trans.png"
+            alt="Hero" 
+          />
+        </div>
+      </section>
+    );
+  };
+  
+  export default Hero;
+  
