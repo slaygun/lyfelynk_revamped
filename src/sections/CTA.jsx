3fb3181803e3b84d22fe94ac3e0873a7245c4840
slaygun <nabhraghav@gmail.com>
2024-06-25 21:37:34 +0530

    cta v1

--- src/sections/CTA.jsx
+++ src/sections/CTA.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import GetInTouch from '../components/GetInTouch';
+import GetStarted from '../components/GetStarted';
 
 const CTA = () => {
   return (
-    <section className='relative py-12 md:py-36 px-6 overflow-hidden'>
-      <img src='/assets/gradient.png' alt="gradient" className='absolute z-[-2] -top-10 left-40 opacity-70 w-auto h-auto max-w-none max-h-none' />
+    <section className='relative py-16 md:py-24 px-6 overflow-hidden'>
       <div className='container mx-auto'>
-        <div className='p-16 mx-auto text-center max-w-6xl h-96 border border-gray-700 bg-blue-700 bg-opacity-20 rounded-3xl'>
-          <h1 className='font-bold font-prompt text-6xl'>Start your Journey with LyfeLynk</h1>
+        <div className='absolute -z-[2] w-full lg:w-1/2 h-full left-0 lg:inset-x-1/4 -bottom-1/2 opacity-80 rounded-full cta__gradient' />
+
+        <div className='relative p-16 md:p-32 mx-auto w-fit border-2 border-gray-700 bg-gray-500  bg-opacity-20 rounded-3xl'>
+            <img 
+                src='/assets/glassheart2.png' 
+                alt='heart in glass' 
+                className='animate-levitate absolute w-32 md:w-48 lg:w-60 -bottom-14 sm:-bottom-16 md:-bottom-24 -right-4 sm:-right-8 md:-right-12 lg:-bottom-24 lg:-right-24'
+            />
+          <h1 className='font-bold font-space-grotesk tracking-tight text-5xl sm:text-8xl'>Start your <br/> health journey <br/> today.</h1>
+          <div className="pt-6 md:pt-12 flex space-x-4">
+            <GetInTouch />
+            <GetStarted />
+          </div>
         </div>
       </div>
     </section>
